a0f74dcc956d54040429765cbad7310336536711
Jose Storopoli <jose@storopoli.com>
2025-09-30 11:16:36 -0300

    fix!: feature-gate serde

--- crates/ssz_types/src/lib.rs
+++ crates/ssz_types/src/lib.rs
@@ -43,9 +43,13 @@
 #[macro_use]
 mod fixed_vector;
 mod optional;
+#[cfg(feature = "serde")]
 pub mod serde_utils;
 mod tree_hash;
 mod variable_list;
+
+#[cfg(any(test, doctest))]
+use serde_json as _;
 
 pub use fixed_vector::FixedVector;
 pub use optional::Optional;
