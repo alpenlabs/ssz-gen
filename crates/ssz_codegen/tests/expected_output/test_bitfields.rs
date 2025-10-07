e59ff335c53beba883f0efc87494ea8cc6aa7df2
Jose Storopoli <jose@storopoli.com>
2025-10-07 16:15:44 -0300

    test: zero-copy codegen generation

--- crates/ssz_codegen/tests/expected_output/test_bitfields.rs
+++ crates/ssz_codegen/tests/expected_output/test_bitfields.rs
@@ -5,6 +5,7 @@ pub mod tests {
             use ssz_derive::{Encode, Decode};
             use tree_hash::TreeHashDigest;
             use tree_hash_derive::TreeHash;
+            use ssz::view::*;
             pub const SMALL_SIZE: u64 = 1u64;
             pub const MEDIUM_SIZE: u64 = 64u64;
             pub const LARGE_SIZE: u64 = 256u64;
@@ -25,6 +26,54 @@ pub mod tests {
                 pub tiny_vec: TinyBitvector,
                 pub std_vec: StandardBitvector,
                 pub large_vec: LargeBitvector,
+            }
+            #[derive(TreeHash)]
+            #[tree_hash(struct_behaviour = "container")]
+            pub struct BitfieldContainerRef<'a> {
+                pub tiny_list: BitListRef<'a, 1usize>,
+                pub std_list: BitListRef<'a, 64usize>,
+                pub large_list: BitListRef<'a, 256usize>,
+                pub tiny_vec: BitVectorRef<'a, 1usize>,
+                pub std_vec: BitVectorRef<'a, 64usize>,
+                pub large_vec: BitVectorRef<'a, 128usize>,
+            }
+            impl<'a> ssz::view::DecodeView<'a> for BitfieldContainerRef<'a> {
+                fn from_ssz_bytes(bytes: &'a [u8]) -> Result<Self, ssz::DecodeError> {
+                    let mut builder = ssz::SszDecoderBuilder::new(bytes);
+                    builder.register_type::<TinyBitlist>()?;
+                    builder.register_type::<StandardBitlist>()?;
+                    builder.register_type::<LargeBitlist>()?;
+                    builder.register_type::<TinyBitvector>()?;
+                    builder.register_type::<StandardBitvector>()?;
+                    builder.register_type::<LargeBitvector>()?;
+                    let mut decoder = builder.build()?;
+                    let tiny_list = decoder.decode_next_view()?;
+                    let std_list = decoder.decode_next_view()?;
+                    let large_list = decoder.decode_next_view()?;
+                    let tiny_vec = decoder.decode_next_view()?;
+                    let std_vec = decoder.decode_next_view()?;
+                    let large_vec = decoder.decode_next_view()?;
+                    Ok(Self {
+                        tiny_list,
+                        std_list,
+                        large_list,
+                        tiny_vec,
+                        std_vec,
+                        large_vec,
+                    })
+                }
+            }
+            impl<'a> BitfieldContainerRef<'a> {
+                pub fn to_owned(&self) -> BitfieldContainer {
+                    BitfieldContainer {
+                        tiny_list: self.tiny_list.to_owned(),
+                        std_list: self.std_list.to_owned(),
+                        large_list: self.large_list.to_owned(),
+                        tiny_vec: self.tiny_vec.to_owned(),
+                        std_vec: self.std_vec.to_owned(),
+                        large_vec: self.large_vec.to_owned(),
+                    }
+                }
             }
         }
     }
--- crates/ssz_codegen/tests/expected_output/test_nested_aliases.rs
+++ crates/ssz_codegen/tests/expected_output/test_nested_aliases.rs
@@ -5,6 +5,7 @@ pub mod tests {
             use ssz_derive::{Encode, Decode};
             use tree_hash::TreeHashDigest;
             use tree_hash_derive::TreeHash;
+            use ssz::view::*;
             pub const SIZE_1: u64 = 10u64;
             pub const SIZE_2: u64 = 10u64;
             pub const SIZE_3: u64 = 10u64;
@@ -23,6 +24,44 @@ pub mod tests {
                 pub field2: E,
                 pub field3: F,
                 pub field4: G,
+            }
+            #[derive(TreeHash)]
+            #[tree_hash(struct_behaviour = "container")]
+            pub struct NestedAliasContainerRef<'a> {
+                pub field1: BytesRef<'a>,
+                pub field2: FixedVectorRef<'a, BytesRef<'a>, 5usize>,
+                pub field3: BytesRef<'a>,
+                pub field4: FixedVectorRef<'a, BytesRef<'a>, 10usize>,
+            }
+            impl<'a> ssz::view::DecodeView<'a> for NestedAliasContainerRef<'a> {
+                fn from_ssz_bytes(bytes: &'a [u8]) -> Result<Self, ssz::DecodeError> {
+                    let mut builder = ssz::SszDecoderBuilder::new(bytes);
+                    builder.register_type::<D>()?;
+                    builder.register_type::<E>()?;
+                    builder.register_type::<F>()?;
+                    builder.register_type::<G>()?;
+                    let mut decoder = builder.build()?;
+                    let field1 = decoder.decode_next_view()?;
+                    let field2 = decoder.decode_next_view()?;
+                    let field3 = decoder.decode_next_view()?;
+                    let field4 = decoder.decode_next_view()?;
+                    Ok(Self {
+                        field1,
+                        field2,
+                        field3,
+                        field4,
+                    })
+                }
+            }
+            impl<'a> NestedAliasContainerRef<'a> {
+                pub fn to_owned(&self) -> NestedAliasContainer {
+                    NestedAliasContainer {
+                        field1: self.field1.to_owned(),
+                        field2: self.field2.to_owned(),
+                        field3: self.field3.to_owned(),
+                        field4: self.field4.to_owned(),
+                    }
+                }
             }
         }
     }
